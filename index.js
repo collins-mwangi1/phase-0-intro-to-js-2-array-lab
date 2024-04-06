9099f01c0e1af4feef957bcee8e9fe7c118b82f4
collins-mwangi1 <collins.mwangi1@student.moringaschool.com>
2024-04-06 18:21:48 +0300

    assignment done

--- index.js
+++ index.js
@@ -30,3 +30,17 @@
       function removeFirstCat() {
         return cats.slice(1);
       }
+      function writeCards(names, event) {
+        const thankYouMessages = [];
+        for (let i = 0; i < names.length; i++) {
+          const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+          thankYouMessages.push(message);
+        }
+        return thankYouMessages;
+      }
+      
+      function countDown(num) {
+        for (let i = num; i >= 0; i--) {
+          console.log(i);
+        }
+      }
